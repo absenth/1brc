8d8760e478ae44889410babb72c61683fd6316ef
absenth <1115138+absenth@users.noreply.github.com>
2024-06-01 21:50:31 -0500

    Day 2 of 100 days of code and the 1brc

--- workdir/src/main.go
+++ workdir/src/main.go
@@ -5,11 +5,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
 func main() {
+
+	datamap := make(map[string][]int64)
 
 	f, err := os.Open("data/sample.txt")
 	if err != nil {
@@ -28,16 +31,37 @@ func main() {
 		itemp := strings.Replace(stemp, ".", "", 1)
 		temp, _ := strconv.ParseInt(itemp, 0, 32)
 
+		// BUG: the parser isn't working quite right for the city
+		// ie. `St. Louis` shows up as `St.`
+
 		/*
 			next steps:
-			 * create a map for the the values using the city as a key
-			 * create a slice as the value for each key in the map
-			 * append the value for each line into the slice
 			 * find min, max, and mean after all lines are read
 			 * divide min/max/mean by 10 to replace my mising deicmal
 		*/
-		fmt.Printf("%s %d\n", city, temp)
+		datamap[city] = append(datamap[city], temp)
 	}
+
+	// NOTE: This sorts the cities into a slice and alphabatizes them
+	keys := make([]string, 0, len(datamap))
+	for key := range datamap {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+
+	// NOTE: "keys" are an alphabetically sorted list of cities from the datamap
+
+	//avgtemp := 0
+	for index, element := range keys {
+		fmt.Printf("%d %s \n", index, element)
+
+		// TODO: Iterate over the map based on the element, in this loop.
+		// TODO: Create the average temp
+		// TODO: Stop Doing It Wrong
+	}
+
+	// NOTE: This should properly print the output...
+	//fmt.Printf("%s = %d %d %d", key, slices.Min(datamap[key]), avg, slices.Max(datamap[key]))
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)

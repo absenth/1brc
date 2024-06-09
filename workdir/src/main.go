49927d7f723d398c26e4dc75d2d31ec1c68cdba6
absenth <1115138+absenth@users.noreply.github.com>
2024-06-08 21:10:29 -0500

    simplifying the logic and rolling the sum function into main

--- workdir/src/main.go
+++ workdir/src/main.go
@@ -14,6 +14,9 @@ import (
 func main() {
 
 	datamap := make(map[string][]int)
+	keys := make([]string, 0, len(datamap))
+	sum := 0
+	count := 0
 
 	f, err := os.Open("data/measurements.txt")
 	if err != nil {
@@ -33,8 +36,6 @@ func main() {
 		datamap[city] = append(datamap[city], int(temp))
 	}
 
-	keys := make([]string, 0, len(datamap))
-
 	for key := range datamap {
 		keys = append(keys, key)
 	}
@@ -44,22 +45,16 @@ func main() {
 	for _, element := range keys {
 		mintemp := float32(slices.Min(datamap[element])) / 10
 		maxtemp := float32(slices.Max(datamap[element])) / 10
-		totaltemp := sum(datamap[element])
-		count := len(datamap[element])
-		meantemp := float32((totaltemp / count)) / 10
+
+		for idx, _ := range datamap[element] {
+			sum += int(datamap[element][idx])
+			count++
+		}
+		meantemp := float32(sum) / 10
 		fmt.Printf("%s=%.1f/%.1f/%.1f, ", element, mintemp, meantemp, maxtemp)
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func sum(arr []int) int {
-
-	var sum int
-	for idx, _ := range arr {
-		sum += arr[idx]
-	}
-	return sum
 }
